24b1ea83bd989bac3ad0884abaaef9efba70c46e
Lahiru Kurukulasuriya <lkurukulasuriya@mitrai.com>
2018-02-27 12:49:09 +0530

    removed app.js server

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 const awsServerlessExpress = require('aws-serverless-express')
 const app = require('./app')
 const userService = require('./userService/user')
-const server = awsServerlessExpress.createServer(app,userService)
+const server = awsServerlessExpress.createServer(userService)
 
 exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context);
